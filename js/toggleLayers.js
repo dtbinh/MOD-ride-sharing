5c12e536ca824d22c0b057cecab5647765a51df8
Danielle Hecht <dhecht@mit.edu>
2017-07-13 14:31:10 +0200

    Create toggleLayers.js

---
+++ js/toggleLayers.js
@@ -0,0 +1,34 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiZGhlY2h0IiwiYSI6ImNqNHRueTVyeDA3ZmYyd3FuY2NmYW9tNmoifQ.FetU2-IBDcrhTmSKBpFIfA';
+var layers = document.getElementById('menu-ui');
+
+addLayer(L.mapbox.tileLayer('mapbox.streets'), 'Base Map', 1);
+addLayer(L.mapbox.tileLayer('mapbox.oneway'), 'High-Demand Areas', 2);
+addLayer(L.mapbox.tileLayer('mapbox.rail-metro'), 'Public Transport Stations', 3);
+     
+function addLayer(layer, name, zIndex) {
+    layer
+        .setZIndex(zIndex)
+        .addTo(map);
+
+    // Create a simple layer switcher that
+    // toggles layers on and off.
+    var link = document.createElement('a');
+        link.href = '#';
+        link.className = 'active';
+        link.innerHTML = name;
+
+    link.onclick = function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        if (map.hasLayer(layer)) {
+            map.removeLayer(layer);
+            this.className = '';
+        } else {
+            map.addLayer(layer);
+            this.className = 'active';
+        }
+    };
+
+    layers.appendChild(link);
+}
