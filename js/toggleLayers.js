650c58425ae6888408bd4fb71a06588404b092b1
Danielle Hecht <dhecht@mit.edu>
2017-07-14 16:02:55 +0200

    Update toggleLayers.js

--- js/toggleLayers.js
+++ js/toggleLayers.js
@@ -1,34 +1,34 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiZGhlY2h0IiwiYSI6ImNqNHRueTVyeDA3ZmYyd3FuY2NmYW9tNmoifQ.FetU2-IBDcrhTmSKBpFIfA';
-var layers = document.getElementById('menu-ui');
+     var layers = document.getElementById('menu-ui');
 
-// addLayer(L.mapbox.tileLayer('mapbox.traffic-night-v2'), 'Night View', 1);
-addLayer(L.mapbox.tileLayer('mapbox.oneway'), 'High-Demand Areas', 1);
-addLayer(L.mapbox.tileLayer('mapbox.rail-metro'), 'Public Transport Stations', 2);
-     
-function addLayer(layer, name, zIndex) {
-    layer
-        .setZIndex(zIndex)
-        .addTo(map);
+     // addLayer(L.mapbox.tileLayer('mapbox.traffic-night-v2'), 'Night View', 1);
+     addLayer(L.mapbox.tileLayer('mapbox.oneway'), 'High-Demand Areas', 1);
+     addLayer(L.mapbox.tileLayer('mapbox.rail-metro'), 'Public Transport Stations', 2);
 
-    // Create a simple layer switcher that
-    // toggles layers on and off.
-    var link = document.createElement('a');
-        link.href = '#';
-        link.className = 'active';
-        link.innerHTML = name;
+     function addLayer(layer, name, zIndex) {
+         layer
+             .setZIndex(zIndex)
+             .addTo(map);
 
-    link.onclick = function(e) {
-        e.preventDefault();
-        e.stopPropagation();
+         // Create a simple layer switcher that
+         // toggles layers on and off.
+         var link = document.createElement('a');
+             link.href = '#';
+             link.className = 'active';
+             link.innerHTML = name;
 
-        if (map.hasLayer(layer)) {
-            map.removeLayer(layer);
-            this.className = '';
-        } else {
-            map.addLayer(layer);
-            this.className = 'active';
-        }
-    };
+         link.onclick = function(e) {
+             e.preventDefault();
+             e.stopPropagation();
+
+             if (map.hasLayer(layer)) {
+                 map.removeLayer(layer);
+                 this.className = '';
+             } else {
+                 map.addLayer(layer);
+                 this.className = 'active';
+             }
+         };
 
     layers.appendChild(link);
 }
