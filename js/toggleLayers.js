ed5297885bbbe0acf0117bf69a57d92456e78950
Danielle Hecht <dhecht@mit.edu>
2017-07-14 15:24:50 +0200

    Update toggleLayers.js

--- js/toggleLayers.js
+++ js/toggleLayers.js
@@ -1,9 +1,9 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiZGhlY2h0IiwiYSI6ImNqNHRueTVyeDA3ZmYyd3FuY2NmYW9tNmoifQ.FetU2-IBDcrhTmSKBpFIfA';
 var layers = document.getElementById('menu-ui');
 
-addLayer(L.mapbox.tileLayer('mapbox.traffic-night-v2'), 'Night View', 1);
-addLayer(L.mapbox.tileLayer('mapbox.oneway'), 'High-Demand Areas', 2);
-addLayer(L.mapbox.tileLayer('mapbox.rail-metro'), 'Public Transport Stations', 3);
+// addLayer(L.mapbox.tileLayer('mapbox.traffic-night-v2'), 'Night View', 1);
+addLayer(L.mapbox.tileLayer('mapbox.oneway'), 'High-Demand Areas', 1);
+addLayer(L.mapbox.tileLayer('mapbox.rail-metro'), 'Public Transport Stations', 2);
      
 function addLayer(layer, name, zIndex) {
     layer
