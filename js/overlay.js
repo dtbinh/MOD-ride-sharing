75e14399417c66fbd8869109ff320cf145ac1fa9
Danielle Hecht <dhecht@mit.edu>
2017-07-14 17:36:57 +0200

    Update overlay.js

--- js/overlay.js
+++ js/overlay.js
@@ -10,8 +10,12 @@ L.mapbox.accessToken = 'pk.eyJ1IjoiZGhlY2h0IiwiYSI6ImNqNHRueTVyeDA3ZmYyd3FuY2NmY
       imageBounds = L.latLngBounds([
         [40.6994, -73.9064],
         [40.8782, -74.0190]]);
-      var map = L.mapbox.map('map','mapbox.streets')
+
+      var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/traffic-day-v2/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGhlY2h0IiwiYSI6ImNqNHRueTVyeDA3ZmYyd3FuY2NmYW9tNmoifQ.FetU2-IBDcrhTmSKBpFIfA')
+      
+      var map = L.mapbox.map('map')
         .fitBounds(imageBounds);
+        .addLayer(tiles)
              // See full documentation for the ImageOverlay type:
              // http://leafletjs.com/reference.html#imageoverlay
       var overlay = L.imageOverlay(imageUrl, imageBounds)
