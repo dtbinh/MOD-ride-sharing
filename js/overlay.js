2c9e7cbeaa055bd106f6313ca86e4d6c07223d73
Danielle Hecht <dhecht@mit.edu>
2017-07-14 16:02:18 +0200

    Update overlay.js

--- js/overlay.js
+++ js/overlay.js
@@ -2,21 +2,21 @@
 
 
 L.mapbox.accessToken = 'pk.eyJ1IjoiZGhlY2h0IiwiYSI6ImNqNHRueTVyeDA3ZmYyd3FuY2NmYW9tNmoifQ.FetU2-IBDcrhTmSKBpFIfA';
-var imageUrl = '/Users/dhecht/Desktop/sim.png',
-      // This is the trickiest part - you'll need accurate coordinates for the
-      // corners of the image. You can find and create appropriate values at
-      // http://maps.nypl.org/warper/ or
-      // http://www.georeferencer.org/
-imageBounds = L.latLngBounds([
-  [40.6994, -73.9064],
-  [40.8782, -74.0190]]);
-var map = L.mapbox.map('map', 'mapbox.streets')
-  .fitBounds(imageBounds);
-       // See full documentation for the ImageOverlay type:
-       // http://leafletjs.com/reference.html#imageoverlay
-var overlay = L.imageOverlay(imageUrl, imageBounds)
-   .addTo(map);
+      var imageUrl = '/Users/dhecht/Desktop/sim.png',
+            // This is the trickiest part - you'll need accurate coordinates for the
+            // corners of the image. You can find and create appropriate values at
+            // http://maps.nypl.org/warper/ or
+            // http://www.georeferencer.org/
+      imageBounds = L.latLngBounds([
+        [40.6994, -73.9064],
+        [40.8782, -74.0190]]);
+      var map = L.mapbox.map('map', 'mapbox.streets')
+        .fitBounds(imageBounds);
+             // See full documentation for the ImageOverlay type:
+             // http://leafletjs.com/reference.html#imageoverlay
+      var overlay = L.imageOverlay(imageUrl, imageBounds)
+         .addTo(map);
 
 
 
-        
+
