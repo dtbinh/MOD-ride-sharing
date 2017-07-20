aee8ba4ee3aaa6a33899665adbb01e1b3a739178
Danielle Hecht <dhecht@mit.edu>
2017-07-20 10:44:19 +0200

    Update vars.js

--- js/vars.js
+++ js/vars.js
@@ -442,7 +442,7 @@ $('#loadTaxis').click(function(){
     $('.overlay').fadeOut(250);
     $('.container').fadeOut(250);
     $('.jumbotron').fadeOut(250);
-    $(' this').fadeOut(250);
+    $( this).fadeOut(250);
     $('.box').fadeIn(250);
     setTimeout(function(){
         updateTimer();
